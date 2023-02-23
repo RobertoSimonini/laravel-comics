11641a1f9c0ea971df3ca4ac9005a3a7e66f25ca
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-23 16:15:42 +0100

    header done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,19 +11,60 @@
 </head>
 <body>
     <header>
-        {{-- logo DC  --}}
-        <figure>
-            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
-        </figure>
-
-        {{-- Qui metto la navbar con i relativi link  --}}
-        <nav>
+        {{-- Qui metto la parte superiore dell'header --}}
+        <div class="header-top">
             <ul>
                 <li>
-                    <a href=""></a>
+                    <a href="#">DC POWER VISA</a>
+                </li>
+                <li>
+                    <a href="#">ADDITIONAL DC SITES</a>
                 </li>
             </ul>
-        </nav>
+        </div>
+        
+        <div class="header-bottom">
+            {{-- logo DC  --}}
+            <figure>
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </figure>
+    
+            {{-- Qui metto la navbar con i relativi link  --}}
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href="">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="">TV</a>
+                    </li>
+                    <li>
+                        <a href="">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="">FANS</a>
+                    </li>
+                    <li>
+                        <a href="">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="">SHOP</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </body>
 </html>
